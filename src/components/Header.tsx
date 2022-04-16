2c7380b0dc718707e17961364fb0fbe1d4c42fff
khaledr <info@khaledr.ir>
2022-04-16 13:19:43 +0430

    develop

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./Header.module.scss";
+
+const Header: React.FunctionComponent = () => {
+  return <header></header>;
+};
+
+export default Header;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HomePage: React.FunctionComponent = () => {
+  return <div>Home page</div>;
+};
+
+export default HomePage;
